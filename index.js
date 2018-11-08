9023cb00b66a78a72e583ac9815532f2ce5fccdb
Louis <ldicarro@mindspring.com>
2018-11-07 19:14:35 -0500

    add updaterepro event handler, add in index.css file

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 window.addEventListener('load', () => {
-    document.querySelector('#user').addEventListener('blur',updateRepros);
-    document.querySelector('#submit').addEventListener('click',getReproEvents);
+    document.querySelector('#user').addEventListener('blur',(evt) => updateRepros(evt));
+    document.querySelector('#submit').addEventListener('click',(evt) => getReproEvents(evt));
 })
 
-function updateRepros() {
-    console.log('blur');
+function updateRepros(evt) {
+    const url = `https://api.github.com/users/${evt.target.value}/repos`;
+
+    fetch(url) 
+        .then((res) => res.json())
+        .then((data) => {
+
+            if(data.length > 0)
+            {
+                let select = document.querySelector('#repros');
+                select.innerHTML = '';
+                data.forEach((el) => {
+                    var tempEl = document.createElement('option');
+                    tempEl.innerText = el.name;
+                    tempEl.value = el.events_url;
+                    console.log(el);
+                    select.appendChild(tempEl);
+                })
+
+                select.removeAttribute('disabled');
+            }
+            else
+            {
+                // display error
+            }
+        })
+
 }
 
-function getReproEvents() {
+function getReproEvents(e) {
     console.log('click');
 }
